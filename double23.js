cf4a7298c59bd7fec7e7c4fa17f776e3f06d40d9
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-18 21:47:24 +0100

    updated

--- double23.js
+++ double23.js
@@ -6,3 +6,15 @@ Examples
 double23([2, 2]) → true
 double23([3, 3]) → true
 double23([2, 3]) → false
+
+function double23(arr){
+    if(arr == 0){
+      return false
+    }else 
+  for(let i =0; i<=2; i++){
+    if(arr[0] === arr[1]){ 
+      return true;
+    }return false;
+  }
+}
+  
